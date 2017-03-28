72399a9e7b932bd7fb9a9920b246437ac97b7bf5
Vinayak Phal <vphal@sapient.com>
2017-03-28 19:39:11 +0530

    Error handling and removed loader for no-results scnario

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from "./components/search-bar";
 import SearchResultList from "./components/search-result-list";
-import Direction from "./components/direction";
+// import Direction from "./components/direction";
 import MapHelper from "./utils/map-helper";
 import Storage from "./utils/storage";
 import config from "./config";
